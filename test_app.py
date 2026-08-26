8be1ac48f5ef2b950f8a7b2cb7be48995daf6410
Maria Fernanda <mariaferrojas189@gmail.com>
2026-08-25 19:57:53 -0500

    test: introduce security vulnerabilities

--- test_app.py
+++ test_app.py
@@ -1,2 +1,8 @@
 def test_ejemplo_basico():
     assert 1 + 1 == 2
+
+
+def test_home():
+    sample_app = __import__("sample_app")
+    response = sample_app.sample.test_client().get("/")
+    assert response.status_code == 200
